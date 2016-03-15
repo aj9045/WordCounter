818c69b64823b3312a099faeb5c03312007cf3d6
aj9045 <identity_unveiled@hotmail.com>
2016-03-15 12:56:44 -0400

    add simple component

---
+++ src/components/ShowCount.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ShowCount extends Component {
+  render() {
+    return (
+      <div>
+        Show count here
+      </div>
+    );
+  }
+}
