661b522cbcd917752ca083c2ea5396b1f16ab122
aj9045 <identity_unveiled@hotmail.com>
2016-03-15 12:56:47 -0400

    add simple component

---
+++ src/components/TextBox.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class TextBox extends Component {
+  render() {
+    return (
+      <div>
+        Text area here
+      </div>
+    );
+  }
+}
