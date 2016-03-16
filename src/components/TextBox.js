37753e052fb3f67191997ef18674cd8a6dbc7a72
aj9045 <identity_unveiled@hotmail.com>
2016-03-16 17:48:14 -0400

    add word to placeholder

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -4,7 +4,7 @@ export default class TextBox extends Component {
   render() {
     return (
       <div className="textBox">
-        <textarea className="textBox__textArea" placeholder="Paste text here" onChange={this.props.changeText}></textarea>
+        <textarea className="textBox__textArea" placeholder="Paste or type text here" onChange={this.props.changeText}></textarea>
       </div>
     );
   }
