07442e374f16cca3031edb81ecba729d121a42b6
aj9045 <identity_unveiled@hotmail.com>
2016-03-16 16:23:29 -0400

    add constructor with state and set text state on change

--- src/index.js
+++ src/index.js
@@ -4,17 +4,25 @@ import TextBox from "./components/TextBox";
 import ShowCount from "./components/ShowCount";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      count: 0,
+    };
+  }
+
   render() {
     return (
       <div>
-        <TextBox changeText={this._changeText} />
+        <TextBox changeText={this._changeText.bind(this)} getCount={this._getCount.bind(this)} />
         <ShowCount />
       </div>
     );
   }
 
   _changeText(event) {
-    console.log(event.target.value);
+    this.setState({text: event.target.value});
   }
 }
 
