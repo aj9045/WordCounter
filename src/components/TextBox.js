f2dcaf4caa333115939d269dc61e08b1c75e3d44
aj9045 <identity_unveiled@hotmail.com>
2016-03-16 16:26:04 -0400

    add class names

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 export default class TextBox extends Component {
   render() {
     return (
-      <div>
-        <textarea placeholder="Paste text here" onChange={this.props.changeText}></textarea>
-        <input type="submit" value="Count" onClick={this.props.getCount} />
+      <div className="textBox">
+        <textarea className="textBox__textArea" placeholder="Paste text here" onChange={this.props.changeText}></textarea>
+        <input className="textBox__button" type="submit" value="Count" onClick={this.props.getCount} />
       </div>
     );
   }
