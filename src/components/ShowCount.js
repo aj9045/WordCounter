0c94c6545445d18755f0d45d826458254256a2d3
aj9045 <identity_unveiled@hotmail.com>
2016-03-16 16:28:03 -0400

    add class names

--- src/components/ShowCount.js
+++ src/components/ShowCount.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 export default class ShowCount extends Component {
   render() {
     return (
-      <div>
-        Word Count: {this.props.count}
+      <div className="showCount">
+        <span className="showCount__wordTitle">Word Count:</span> <span className="showCount__wordCount">{this.props.count}</span>
       </div>
     );
   }
